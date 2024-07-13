60230542c11a25d7cbfcd0260a9db6eb38415b98
Bernard Ponce <poncebernard123@gmail.com>
2024-07-13 22:21:50 +0800

    Add CORS Middleware w/ simple wildcard * (That can be toggle by commenting/uncommenting it) for testing using the HTML w/ Pure Javascript Script File.

--- main.py
+++ main.py
@@ -6,11 +6,26 @@ from config.database import get_db, initialize_setup, engine, Base
 initialize_setup()
 
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from api.v1.routers.main_route import router as v1_router
 
 app = FastAPI()
+
+origins = [
+    "*", #Comment this before uploading to server
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(v1_router, dependencies=[Depends(get_db)])
 
