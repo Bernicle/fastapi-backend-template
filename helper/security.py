9d11a1e4cb9a3540c8adef82cb4316544c3c1396
Bernard Ponce <poncebernard123@gmail.com>
2024-07-14 20:30:09 +0800

    Adjust the helper/security.py for encrypting/decrypting password

--- helper/security.py
+++ helper/security.py
@@ -1,15 +1,11 @@
 from bcrypt import hashpw, gensalt
 
-def hash_password(password):
+def hash_password(password : str) -> str:
    password_bytes = password.encode('utf-8')
    generate_salt = gensalt()
    hashed_bytes = hashpw(password_bytes, generate_salt)
-   return f"{hashed_bytes.decode('utf-8')}:{generate_salt}"
+   return f"{hashed_bytes.decode('utf-8')}:{generate_salt.decode('utf-8')}"
 
-def verify_password(password, hash_password):
+def verify_password(password : str, hash_password : str) -> bool:
    hash_code, salt = hash_password.split(':')
-#    print(password)
-#    print(hash_password)
-#    print(hash_code)
-#    print(salt)
    return (hash_code == hashpw(password.encode('utf-8'), salt.encode('utf-8')).decode('utf-8'))
