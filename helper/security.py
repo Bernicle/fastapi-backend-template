b51dee68de895931a42123b69905c25ecb983e66
Bernard Ponce <poncebernard123@gmail.com>
2024-07-15 23:37:30 +0800

    Add Create Access Token inside the helper/security.py and also create a simple test_security.py to auto simulate the testing of security.py basic function (hash_password, verify_password)

--- helper/security.py
+++ helper/security.py
@@ -1,11 +1,31 @@
 from bcrypt import hashpw, gensalt
+from typing import Union
+from datetime import datetime, timedelta, timezone
+import jwt
 
 def hash_password(password : str) -> str:
+   if not isinstance(password, str):
+      raise TypeError("Input must be a string.")
+   
    password_bytes = password.encode('utf-8')
    generate_salt = gensalt()
    hashed_bytes = hashpw(password_bytes, generate_salt)
    return f"{hashed_bytes.decode('utf-8')}:{generate_salt.decode('utf-8')}"
 
 def verify_password(password : str, hash_password : str) -> bool:
+   if not isinstance(password, str):
+      raise TypeError("Input must be a string.")
+   if not isinstance(hash_password, str):
+      raise TypeError("Input must be a string.")
    hash_code, salt = hash_password.split(':')
    return (hash_code == hashpw(password.encode('utf-8'), salt.encode('utf-8')).decode('utf-8'))
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None, SECRET_SETTING : dict = {}):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_SETTING.get('SECRET_KEY'), algorithm=SECRET_SETTING.get('ALGORITHM'))
+    return encoded_jwt
