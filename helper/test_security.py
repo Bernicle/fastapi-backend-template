4b4aece65787ce9456503dbf8219e5a00391726c
Bernard Ponce <poncebernard123@gmail.com>
2024-07-17 23:15:17 +0800

    Modify the simple test unit (helper/test_security.py) for helper/security.py. This including datatype for each variable, Separatng the Test Unit for 'happy scenario' and 'error handling' inside the one file (helper/test_security.py

--- helper/test_security.py
+++ helper/test_security.py
@@ -2,33 +2,41 @@ from helper.security import hash_password, verify_password, create_access_token
 import pytest
 
 def test_password_hashing():
-    test_value = "Pythonamic"
+    test_value :str = "Pythonamic"
     hash_value = hash_password(test_value)
     assert isinstance(hash_value, (str)), "The data type receive must be in String."
     assert verify_password(test_value, hash_value), "Failed to verify the value."
 
-    test_value = "1234passwOrd.!"
+    test_value :str  = "1234passwOrd.!"
     hash_value = hash_password(test_value)
     assert isinstance(hash_value, (str)), "The data type receive must be in String."
     assert verify_password(test_value, hash_value), "Failed to verify the value."
     
-    test_value = "1234passwiiiiiiiiiiiodjfakldjf sa.f asfd. saf jof  df!iiiiasdf af123 1r  afssgewg 346 2rfe fdsiiiiOrd.!"
+    test_value :str  = "1234passwiiiiiiiiiiiodjfakldjf sa.f asfd. saf jof  df!iiiiasdf af123 1r  afssgewg 346 2rfe fdsiiiiOrd.!"
     hash_value = hash_password(test_value)
     assert isinstance(hash_value, (str)), "The data type receive must be in String."
     assert verify_password(test_value, hash_value), "Failed to verify the value."
     
-    
+def test_invalid_input():    
     with pytest.raises(TypeError):
-        test_value = 123123
-        hash_value = hash_password(test_value)
+        test_value : int = 123123
+        _ = hash_password(test_value)
 
     with pytest.raises(TypeError):
         test_value = b'01001111011010101001011010101010'
         _ = hash_password(test_value)
+
+    with pytest.raises(TypeError):
+        test_value : bool = True
+        _ = hash_password(test_value)
+
+    with pytest.raises(TypeError):
+        test_value = None
+        _ = hash_password(test_value)
         
     with pytest.raises(TypeError):
-        test_value = 123918273
-        hash_value = 19280219824902813
+        test_value : int = 123918273
+        hash_value : int = 19280219824902813
         _ = verify_password(test_value, hash_value)
         
     
