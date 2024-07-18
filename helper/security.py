229eeda5934de8936eec964a36171b69cd47ca7c
Bernard Ponce <poncebernard123@gmail.com>
2024-07-19 07:21:37 +0800

    Update the helper/security.py and helper/test_security.py to include the function for creating and verifying of access_token using JwT and create simple test unit for that function

--- helper/security.py
+++ helper/security.py
@@ -4,21 +4,21 @@ from datetime import datetime, timedelta, timezone
 import jwt
 
 def hash_password(password : str) -> str:
-   if not isinstance(password, str):
-      raise TypeError("Input must be a string.")
+    if not isinstance(password, str):
+        raise TypeError("Input must be a string.")
    
-   password_bytes = password.encode('utf-8')
-   generate_salt = gensalt()
-   hashed_bytes = hashpw(password_bytes, generate_salt)
-   return f"{hashed_bytes.decode('utf-8')}:{generate_salt.decode('utf-8')}"
+    password_bytes = password.encode('utf-8')
+    generate_salt = gensalt()
+    hashed_bytes = hashpw(password_bytes, generate_salt)
+    return f"{hashed_bytes.decode('utf-8')}:{generate_salt.decode('utf-8')}"
 
 def verify_password(password : str, hash_password : str) -> bool:
-   if not isinstance(password, str):
-      raise TypeError("Input must be a string.")
-   if not isinstance(hash_password, str):
-      raise TypeError("Input must be a string.")
-   hash_code, salt = hash_password.split(':')
-   return (hash_code == hashpw(password.encode('utf-8'), salt.encode('utf-8')).decode('utf-8'))
+    if not isinstance(password, str):
+        raise TypeError("Input must be a string.")
+    if not isinstance(hash_password, str):
+        raise TypeError("Input must be a string.")
+    hash_code, salt = hash_password.split(':')
+    return (hash_code == hashpw(password.encode('utf-8'), salt.encode('utf-8')).decode('utf-8'))
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None, SECRET_SETTING : dict = {}):
     to_encode = data.copy()
@@ -27,5 +27,12 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_SETTING.get('SECRET_KEY'), algorithm=SECRET_SETTING.get('ALGORITHM'))
+    encoded_jwt = jwt.encode(payload=to_encode,key=SECRET_SETTING.get('SECRET_KEY'), algorithm=SECRET_SETTING.get('ALGORITHM'))
     return encoded_jwt
+
+
+def decode_access_token(encoded_jwt: str, SECRET_SETTING : dict = {}) -> dict:
+    
+    decoded_jwt = jwt.decode(encoded_jwt, SECRET_SETTING.get('SECRET_KEY'), algorithms=SECRET_SETTING.get('ALGORITHM'))
+    
+    return decoded_jwt
--- helper/test_security.py
+++ helper/test_security.py
@@ -1,4 +1,4 @@
-from helper.security import hash_password, verify_password, create_access_token
+from helper.security import hash_password, verify_password, create_access_token, decode_access_token
 import pytest
 
 def test_password_hashing():
@@ -39,4 +39,23 @@ def test_invalid_input():
         hash_value : int = 19280219824902813
         _ = verify_password(test_value, hash_value)
         
+def test_token_generation():
+    user = {
+        "id": 15,
+        "name": "Lorem Ipsum",
+        "age": 28
+    }
+    dummy_secret = {
+        "SECRET_KEY":"3dWdasgSdpSi0/D4qtaZ31BVg8t6/pq5j6VkPt6ucZA=",
+        "ALGORITHM": "HS256"
+    }
     
+    access_token = create_access_token(data=user, SECRET_SETTING=dummy_secret)
+    assert access_token != None
+    assert isinstance(access_token, (str))
+
+    decoded_token = decode_access_token(encoded_jwt=access_token, SECRET_SETTING=dummy_secret)
+    assert isinstance(decoded_token, (dict))
+    assert decoded_token.keys() 
+    assert "exp" in decoded_token.keys()
+    assert set(user.keys()).issubset(set(decoded_token.keys()))
