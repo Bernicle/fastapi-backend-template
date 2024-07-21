2e71ce4c1a46879f7e4e8311b5bf3c8214852f53
Bernard Ponce <poncebernard123@gmail.com>
2024-07-21 14:33:10 +0800

    Update the item_service.py to separate the Businuss Logic from Controller

--- api/v1/controllers/module1/item_controller.py
+++ api/v1/controllers/module1/item_controller.py
@@ -19,50 +19,28 @@ async def get_items(item_service : ItemService = Depends(get_item_service)):
     return await item_service.get_all_item()
 
 @router.get("/{item_id}", response_model=item_schema, responses={400: {"description": "Item not found", "model": InvalidIDResponse}})
-async def get_item_by_id(item_id: int, db: Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+async def get_item_by_id(item_id: int, item_service : ItemService = Depends(get_item_service), current_user : User = Depends(get_current_user)):
     # ... (Retrieve item by ID from database)
-    item = db.query(Item).filter(Item.id  == item_id).first()
+    item = item_service.get_item(item_id=item_id)
     if not item:
         raise HTTPException(status_code=400, detail=f"The item with provided ID was not exist. Try different ID.")
     
     return item
 
 @router.post("/", response_model=item_schema, status_code=status.HTTP_201_CREATED)
-async def create_item(item: item_create_schema, db: Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+async def create_item(item: item_create_schema, item_service : ItemService = Depends(get_item_service), current_user : User = Depends(get_current_user)):
     # Create a new Item instance using Pydantic data
-    new_item = Item(**item.model_dump())  # Unpack the Pydantic data
-    db.add(new_item)
-    db.commit()
-    db.refresh(new_item)  # Refresh the object to include the newly generated ID
-
+    new_item = await item_service.create_item(item=item)
     return new_item
 
 @router.put("/{item_id}", response_model=item_schema, responses={404: {"model": InvalidIDResponse}})
-async def update_item(item_id: int, item_data: item_update_schema, db: Session = Depends(get_db)):
+async def update_item(item_id: int, item_data: item_update_schema, item_service : ItemService = Depends(get_item_service)):
     # Retrieve the item by ID
-    item = db.query(Item).filter(Item.id == item_id).first()
-    if not item:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-    # Update the item's attributes with the provided data
-    for field, value in item_data.model_dump(exclude_unset=True).items():
-        setattr(item, field, value)  # Update specific attributes as needed
-    
-    db.commit()
-    db.refresh(item)  # Refresh for updated values
-
+    item = item_service.update_item(item_id=item_id, item_data=item_data)
     return item
 
 @router.delete("/{item_id}", status_code=status.HTTP_204_NO_CONTENT, responses={404: {"model": InvalidIDResponse}})
-async def delete_item(item_id: int, db: Session = Depends(get_db)):
-    # Retrieve the item by ID
-    item = db.query(Item).filter(Item.id == item_id).first()
-    if not item:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-    # Delete the item from the database
-    db.delete(item)
-    db.commit()
-
+async def delete_item(item_id: int, item_service : ItemService = Depends(get_item_service)):
+    item_service.delete_item(item_id=item_id)
     # Return no content (204) on successful deletion
     return None
--- api/v1/services/module1/item_service.py
+++ api/v1/services/module1/item_service.py
@@ -10,29 +10,49 @@ class ItemService:
         self.db = db
 
     async def get_all_item(self):
-        # Implement logic for user creation (validation, password hashing, saving to database)
-        # ...
+        # Implement logic for fetching all item
         return self.db.query(Item).all()
     
-    async def create_item(self, item_user: item_create_schema):
-        # Implement logic for user creation (validation, password hashing, saving to database)
-        # ...
-        pass
+    async def create_item(self, item: item_create_schema):
+        # Implement logic for item creation (validation, saving to database)
+        new_item = Item(**item.model_dump())  # Unpack the Pydantic data
+        self.db.add(new_item)
+        self.db.commit()
+        self.db.refresh(new_item)  # Refresh the object to include the newly generated ID
+        return new_item
 
     def get_item(self, item_id: int):
-        # Implement logic for fetching a specific user by ID
-        # ...
-        pass
+        # Implement logic for fetching a specific item by ID
+        return self.db.query(Item).filter(Item.id  == item_id).first()
 
-    def update_item(self, user_id: int, item_data: Item):
-        # Implement logic for updating user data
-        # ...
-        pass
+    def update_item(self, item_id: int, item_data: Item):
+        # Implement logic for updating item data
+        
+        item = self.db.query(Item).filter(Item.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        # Update the item's attributes with the provided data
+        for field, value in item_data.model_dump(exclude_unset=True).items():
+            setattr(item, field, value)  # Update specific attributes as needed
+        
+        self.db.commit()
+        self.db.refresh(item)  # Refresh for updated values
+        return item
+
 
     def delete_item(self, item_id: int):
-        # Implement logic for deleting a user
-        # ...
-        pass
+        # Implement logic for deleting a item
+        # Retrieve the item by ID
+        item = self.db.query(Item).filter(Item.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        # Delete the item from the database
+        self.db.delete(item)
+        self.db.commit()
+        
+
 
 # Dependency for injecting the database session
 def get_item_service(db: Session = Depends(get_db)):
